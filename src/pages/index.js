a44d322a45c0e7230db154c8225d01719bb83ca5
Carlos Vitor <43506171+CarlosVitr@users.noreply.github.com>
2020-09-05 09:02:57 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function Index() {
         <title> Gatsby + Node.js (TypeScript) API</title>
       </Helmet>
       <h1>Iniciando Projeto meu site Gatsby + Node.js (TypeScript) API</h1>
-      <h2> Site de <a href="https://github.com/carlosvitr" target="_blank">Carlos</a></h2>
+      <h2> Site de <a href="https://github.com/carlosvitr/site-blog-Carlos" target="_blank">Carlos</a></h2>
       <h2>
         Deployed with{' '}
         <a
